407718c47b0e194f8abb46ae61eb77e6def85e87
tk2u <tkay2u@gmail.com>
2018-10-18 20:32:04 -0500

    R script prototype and test data
    Data Wearhouse is the flat file of test data. AnalysisScript.R is the
    initial code written early 2016 by TK and SG.

---
+++ TurnStyle/AnalysisScript.R
@@ -0,0 +1,31 @@
+data = read.csv("C:/Users/tashakay109/Documents/TheDataWearhouse.csv")
+mod.data=data
+newlabels = c('Top','Dress','IDK','IDK','IDK','Bot','IDK','Top','Top')
+mod.data$Type = factor(mod.data$Item,labels = newlabels)
+ 
+for (outfit in 1:10){
+  outfit_flag=0
+  while (outfit_flag==0){
+    
+    top.index=sample(nrow(mod.data),1)
+    bot.index=sample(nrow(mod.data),1)
+    
+    top=mod.data$Type[sample(nrow(mod.data),1)]
+    bot=mod.data$Type[sample(nrow(mod.data),1)]
+
+  
+  if (top=="Top" & bot=="Bot") {
+  
+      outfit_flag=1
+      print("found_outfit")
+      print("----Top----")
+      print(mod.data$Color[top.index])
+      print(mod.data$Item[top.index])
+      print(mod.data$Occasion[top.index])
+      print("----Bottom----")
+      print(mod.data$Color[bot.index])
+      print(mod.data$Item[bot.index])
+      print(mod.data$Occasion[bot.index])
+   }
+  }
+} 
